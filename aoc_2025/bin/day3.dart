64f5428c8af17161fc62a4ff5b1db4a79e04314a
Dan Carroll <dancarroll@gmail.com>
2025-12-03 16:55:45 -0500

    2025 Day 3 skeleton

---
+++ aoc_2025/bin/day3.dart
@@ -0,0 +1,12 @@
+import 'package:aoc_2025/day3/part_1.dart' as part1;
+import 'package:aoc_2025/day3/part_2.dart' as part2;
+import 'package:aoc_shared/shared.dart';
+
+Future<void> main(List<String> arguments) async {
+  await runDay(
+    year: Year.y2025,
+    day: Day.day3,
+    part1: part1.calculate,
+    part2: part2.calculate,
+  );
+}
---
+++ aoc_2025/lib/day3/part_1.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'shared.dart';
+
+Future<int> calculate(File file) async {
+  final input = await loadData(file);
+  return 0;
+}
