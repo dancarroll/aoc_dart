50d61bbdbf07d3e3538c96deeeb9fd0fa9e60107
Dan Carroll <dancarroll@gmail.com>
2025-12-04 22:16:04 -0500

    2025 Day 4 Part 1 refactoring to use a sparse map, in preparation for Part 2

--- aoc_2025/lib/day4/part_1.dart
+++ aoc_2025/lib/day4/part_1.dart
@@ -10,41 +10,6 @@ import 'shared.dart';
 /// in neighboring squares (all eight cardinal positions). Return
 /// the number of accessible rolls.
 Future<int> calculate(File file) async {
-  final input = await loadData(file);
-
-  int numAccessibleRolls = 0;
-  for (int r = 0; r < input.length; r++) {
-    for (int c = 0; c < input[0].length; c++) {
-      if (input[r][c] == '.') {
-        continue;
-      }
-
-      final neighbors = numNeighbors(input, r, c);
-      if (neighbors < 4) {
-        numAccessibleRolls++;
-      }
-    }
-  }
-
-  return numAccessibleRolls;
-}
-
-/// Return the number of neighbors for the given map and point.
-int numNeighbors(List<List<String>> map, int r, int c) {
-  return [
-        (r - 1, c - 1),
-        (r - 1, c),
-        (r - 1, c + 1),
-        (r, c - 1),
-        (r, c + 1),
-        (r + 1, c - 1),
-        (r + 1, c),
-        (r + 1, c + 1),
-      ]
-      .where((pos) {
-        final (nr, nc) = pos;
-        return nr >= 0 && nr < map.length && nc >= 0 && nc < map[0].length;
-      })
-      .where((pos) => map[pos.$1][pos.$2] == '@')
-      .length;
+  final map = await loadData(file);
+  return map.points.where(map.isAccessible).length;
 }
--- aoc_2025/lib/day4/shared.dart
+++ aoc_2025/lib/day4/shared.dart
@@ -1,6 +1,53 @@
 import 'dart:io';
+import 'dart:math';
 
-Future<List<List<String>>> loadData(File file) async {
+/// Represents the map of the printing department described in the problem.
+final class Map {
+  final Set<Point<int>> points;
+  final int rows;
+  final int cols;
+
+  Map(this.points, this.rows, this.cols);
+
+  /// Returns if the point is accessible (fewer than 4 neighbors in the
+  /// eigth cardinal positions around the point).
+  bool isAccessible(Point<int> point) => numNeighbors(point) < 4;
+
+  /// Return the number of neighbors for the given map and point.
+  int numNeighbors(Point<int> point) {
+    int r = point.x;
+    int c = point.y;
+    return [
+          (r - 1, c - 1),
+          (r - 1, c),
+          (r - 1, c + 1),
+          (r, c - 1),
+          (r, c + 1),
+          (r + 1, c - 1),
+          (r + 1, c),
+          (r + 1, c + 1),
+        ]
+        .where((pos) {
+          final (nr, nc) = pos;
+          return nr >= 0 && nr < rows && nc >= 0 && nc < cols;
+        })
+        .where((pos) => points.contains(Point(pos.$1, pos.$2)))
+        .length;
+  }
+}
+
+Future<Map> loadData(File file) async {
   final lines = await file.readAsLines();
-  return lines.map((l) => l.split('').toList()).toList();
+
+  Set<Point<int>> map = {};
+  for (int r = 0; r < lines.length; r++) {
+    final row = lines[r].split('');
+    for (int c = 0; c < row.length; c++) {
+      if (row[c] == '@') {
+        map.add(Point(r, c));
+      }
+    }
+  }
+
+  return Map(map, lines.length, lines[0].length);
 }
