78a34d8e97fab5735646fee561748d5e545c8afc
Dan Carroll <dancarroll@gmail.com>
2025-12-12 23:00:09 -0500

    Share logic between the 2025 Day 11 parts

--- aoc_2025/lib/day11/part_2.dart
+++ aoc_2025/lib/day11/part_2.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:collection/collection.dart';
-
 import 'shared.dart';
 
 /// Following from Part 1, we now need to start at the `svr` node
@@ -16,50 +14,4 @@ Future<int> calculate(File file) async {
     requiredNodesInPath: {'dac', 'fft'},
     cachedValues: {},
   );
-}
-
-/// Recursive function to determine the number of paths from the given node
-/// to the 'out' node, only allowing a single visit to any given node, and
-/// requiring the given nodes to be in the path at some point.
-int numPathsToEnd({
-  required String node,
-  required Map<String, Set<String>> graph,
-  required Set<String> requiredNodesInPath,
-  required Map<String, int> cachedValues,
-}) {
-  if (node == 'out') {
-    // Path is only valid if we visited the required nodes.
-    if (requiredNodesInPath.isEmpty) {
-      return 1;
-    }
-    return 0;
-  }
-
-  // Check to see if we've already processed the node at a similar
-  // point in its path (still needing the specified nodes).
-  // Use a string-only cache key, which performs much better
-  // than a record.
-  final cacheKey = '$node${requiredNodesInPath.join('')}';
-  final cachedValue = cachedValues[cacheKey];
-  if (cachedValue != null) {
-    return cachedValue;
-  }
-
-  int numPaths = 0;
-  final nextNodes = graph[node] ?? {};
-  for (final next in nextNodes) {
-    numPaths += numPathsToEnd(
-      node: next,
-      graph: graph,
-      // If the next node is one of the required nodes, remove it from the
-      // required node set in this branch.
-      requiredNodesInPath: requiredNodesInPath
-          .whereNot((s) => s == next)
-          .toSet(),
-      cachedValues: cachedValues,
-    );
-  }
-
-  cachedValues[cacheKey] = numPaths;
-  return numPaths;
 }
--- aoc_2025/lib/day11/shared.dart
+++ aoc_2025/lib/day11/shared.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+
 Future<Map<String, Set<String>>> loadData(File file) async {
   final lines = await file.readAsLines();
 
@@ -12,4 +14,50 @@ Future<Map<String, Set<String>>> loadData(File file) async {
   }
 
   return graph;
+}
+
+/// Recursive function to determine the number of paths from the given node
+/// to the 'out' node, only allowing a single visit to any given node, and
+/// requiring the given nodes to be in the path at some point.
+int numPathsToEnd({
+  required String node,
+  required Map<String, Set<String>> graph,
+  required Set<String> requiredNodesInPath,
+  required Map<String, int> cachedValues,
+}) {
+  if (node == 'out') {
+    // Path is only valid if we visited the required nodes.
+    if (requiredNodesInPath.isEmpty) {
+      return 1;
+    }
+    return 0;
+  }
+
+  // Check to see if we've already processed the node at a similar
+  // point in its path (still needing the specified nodes).
+  // Use a string-only cache key, which performs much better
+  // than a record.
+  final cacheKey = '$node${requiredNodesInPath.join('')}';
+  final cachedValue = cachedValues[cacheKey];
+  if (cachedValue != null) {
+    return cachedValue;
+  }
+
+  int numPaths = 0;
+  final nextNodes = graph[node] ?? {};
+  for (final next in nextNodes) {
+    numPaths += numPathsToEnd(
+      node: next,
+      graph: graph,
+      // If the next node is one of the required nodes, remove it from the
+      // required node set in this branch.
+      requiredNodesInPath: requiredNodesInPath
+          .whereNot((s) => s == next)
+          .toSet(),
+      cachedValues: cachedValues,
+    );
+  }
+
+  cachedValues[cacheKey] = numPaths;
+  return numPaths;
 }
